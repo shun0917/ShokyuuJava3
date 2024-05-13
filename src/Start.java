cbcb2d0ac6b1abbb99e66b0544b795e4a8fd1eb6
takagi eishun <shun0534@icloud.com>
2024-05-13 11:55:41 +0900

    初級Java　プロジェクトのコミット

---
+++ src/Start.java
@@ -0,0 +1,27 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int x=2;
+		int y=3;
+		System.out.println(x*y);
+		
+		MyFrame frame1=new RectFrame();
+		frame1.setLocation(300,100);
+		frame1.setSize(350,550);
+		
+		MyFrame frame2=new RectFrame();
+		frame2.setLocation(425,100);
+		frame2.setSize(350,550);
+
+		
+		
+		
+		
+
+
+	}
+
+}
