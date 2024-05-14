99b6d7104d58370509a5a4d8fa90d721181793a1
takagi eishun <shun0534@icloud.com>
2024-05-14 12:10:53 +0900

    3.3さまざまな色で描く

--- 初級Java2.5/src/RectTest.java
+++ 初級Java2.5/src/RectTest.java
@@ -1,12 +1,26 @@
 
 public class RectTest extends MyFrame {
 	public void run() {
+		
+		//紫
+		setColor(128,0,128);
 		fillRect(30,40,50,100);
+		
+		//水色
+		setColor(173,216,230);
 		fillRect(130,40,50,100);
+		
+		//黒
+		setColor(0,0,0);
 		fillRect(30,200,150,10);
 		
-		fillRect(90,160,30,30);//hana
-
+		//黄色
+		setColor(255,255,0);
+		fillRect(90,160,30,30);//鼻
+		
+		//灰色
+		setColor(128,128,128);
+		fillRect(160,200,20,30);//口右
 		
 
 
