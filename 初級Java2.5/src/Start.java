f55063550c11cd8e64534f8b54f49ee8277bd252
takagi eishun <shun0534@icloud.com>
2024-05-14 10:45:20 +0900

    3.1塗りつぶし四角形を描く

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -3,25 +3,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
-		int x=2;
-		int y=3;
-		System.out.println(x*y);
-		
-		MyFrame frame1=new RectFrame();
-		frame1.setLocation(300,100);
-		frame1.setSize(350,550);
-		
-		MyFrame frame2=new RectFrame();
-		frame2.setLocation(425,100);
-		frame2.setSize(350,550);
-
-		
-		
-		
-		
-
-
+		System.out.println("プログラムを実行します");
+		MyFrame frame1=new RectTest();
 	}
 
 }
