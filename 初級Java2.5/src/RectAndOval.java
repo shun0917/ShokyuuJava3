d49328e80e3f634306d8e68d644bbccf2086f9cc
takagi eishun <shun0534@icloud.com>
2024-05-20 10:32:33 +0900

    3-1.練習問題

---
+++ 初級Java2.5/src/RectAndOval.java
@@ -0,0 +1,11 @@
+
+public class RectAndOval extends MyFrame{
+	public void run() {
+		fillRect(100,100,40,20);
+		fillRect(70,110,100,20);
+		fillOval(90,125,20,20);
+		fillOval(135,125,20,20);
+	}
+
+
+}
