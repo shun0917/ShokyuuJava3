d8a6db86363b6de85bb072a8b86f8defaa290df8
takagi eishun <shun0534@icloud.com>
2024-05-21 10:51:32 +0900

    練習問題4-3

---
+++ 初級Java2.5/src/RectIf.java
@@ -0,0 +1,23 @@
+;
+public class RectIf extends MyFrame{
+	public void run() {
+		
+		int x=30;
+        
+		int color =0;
+		
+		int i;
+		for(i=0; i<10;i++) {
+			
+			setColor(color,color,color);
+    
+            fillRect(x,50,10,100);
+            	
+			x+=20;
+			color+=20;
+
+		}
+		
+	}
+	
+}
