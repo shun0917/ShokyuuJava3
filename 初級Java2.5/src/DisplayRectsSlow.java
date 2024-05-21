5779003ac0eca6b7e82be4c67e2f7ba649765417
takagi eishun <shun0534@icloud.com>
2024-05-21 12:01:48 +0900

    確認問題5.1

--- 初級Java2.5/src/DisplayRectsSlow.java
+++ 初級Java2.5/src/DisplayRectsSlow.java
@@ -4,7 +4,7 @@ public class DisplayRectsSlow extends MyFrame {
 		setColor(0,128,0);
 		int x=30;
 		while (x<=170) {
-			fillRect(x,80,10,100);
+			fillRect(x,80+x,10,100);
 			x+=20;
 			sleep(1);
 			
