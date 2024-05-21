bd87b4da4833cedeb6979da99094817c5188fb80
takagi eishun <shun0534@icloud.com>
2024-05-21 09:49:16 +0900

    4.4 if文を利用する

--- 初級Java2.5/src/RectIf.java
+++ 初級Java2.5/src/RectIf.java
@@ -1,20 +1,23 @@
-
+;
 public class RectIf extends MyFrame{
 	public void run() {
 		
 		int x=30;
 
-		int color =0;
+		setColor (0,128,0);
 		
 		int i;
-		for(i=0; i<10;i++) {
-			
-			setColor(color,color,color);
-
-			fillRect(x,50,10,100);
+		for(i=0; i<9;i++) {
+            if(i >= 4 && i <=7) {
+                fillRect(x,100,10,100);
+			}
+            else {
+    			fillRect(x,80,10,100);
+            	
+            }
 			
 			x+=20;
-			color+=20;
+
 		}
 		
 
