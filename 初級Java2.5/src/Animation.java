0fbcee40e5853677f63be01b669cdd14ee005d35
takagi eishun <shun0534@icloud.com>
2024-05-27 12:20:14 +0900

    おまけ問題5-1

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -3,15 +3,25 @@ public class Animation extends MyFrame {
 	public void run() {
 		int y = 100;
 		int x = 30;
+
 		while (true) {
-			while (x <= 170) {
+			while (x <= 150) {
 				clear();
 				setColor(0, 128, 0);
 				fillRect(x, y, 10, 100);
 				x += 5;
 				sleep(0.1);
-
 			}
+
+			int nextY = y+25;
+			while (y <= nextY) {
+				clear();
+				setColor(0, 128, 0);
+				fillRect(x, y, 10, 100);
+				y += 5;
+				sleep(0.1);
+			}
+
 			while (x >= 30) {
 				clear();
 				setColor(0, 128, 0);
@@ -19,7 +29,17 @@ public class Animation extends MyFrame {
 				x -= 5;
 				sleep(0.1);
 			}
-		}
-	}
+			
+			int nextYY = y+25;
+			while (y <= nextYY) {
+				clear();
+				setColor(0, 128, 0);
+				fillRect(x, y, 10, 100);
+				y += 5;
+				sleep(0.1);
+			}
 
+		}
+
+	}
 }
