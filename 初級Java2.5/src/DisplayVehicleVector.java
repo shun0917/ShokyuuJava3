2772692f4f7b100370c93627c39639de65088a44
髙木　瑛俊 <133090478+shun0917@users.noreply.github.com>
2024-06-10 12:00:18 +0900

    練習問題 8-1

--- 初級Java2.5/src/DisplayVehicleVector.java
+++ 初級Java2.5/src/DisplayVehicleVector.java
@@ -3,9 +3,12 @@ import java.util.Vector;
 public class DisplayVehicleVector extends MyFrame{
 	public void run() {
 		Vector<Vehicle> vehicles=new Vector<Vehicle>();
-		vehicles.add(new Car(10,30,3,0));
-		vehicles.add(new Car(10,100,5,0));
-		vehicles.add(new Train(10,150,7,0));
+		vehicles.add(new Car(180,30,3,0));
+		vehicles.add(new Car(180,100,-5,0));
+		vehicles.add(new Train(180,160,14,0));
+		vehicles.add(new Train(180,195,-12,0));
+		vehicles.add(new Truck(180,230,8,0));
+		vehicles.add(new Truck(180,275,6,0));
 		
 		for (int i=0; i<30; i++) {
 			clear();
