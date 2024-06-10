db8958a4018d0ca5610190dac8e19c6a88c45904
髙木　瑛俊 <133090478+shun0917@users.noreply.github.com>
2024-06-10 11:32:10 +0900

    8.4複数の種類の図形を動かす

--- 初級Java2.5/src/Vehicle.java
+++ 初級Java2.5/src/Vehicle.java
@@ -9,9 +9,15 @@ public class Vehicle {
 		this.vx = vx;
 		this.vy = vy;
 	}
+	
 	public void move() {
 		x+=vx;
 		y+=vy;
+	}
+	
+	public void draw(MyFrame f) {
+		// TODO 自動生成されたメソッド・スタブ
+		
 	}
 
 }
