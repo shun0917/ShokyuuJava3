db677220db28b734b5277641e7abeb6ea461cbd3
髙木　瑛俊 <133090478+shun0917@users.noreply.github.com>
2024-06-10 10:44:00 +0900

    8.3共通する性質を抜き出す（継承）

--- 初級Java2.5/src/Train.java
+++ 初級Java2.5/src/Train.java
@@ -1,12 +1,9 @@
 
-public class Train {
-	int x, y, vx, vy;
+public class Train extends Vehicle{
 
 	public Train(int x, int y, int vx, int vy) {
-		this.x = x;
-		this.y = y;
-		this.vx = vx;
-		this.vy = vy;
+		super(x, y, vx, vy);
+		// TODO 自動生成されたコンストラクター・スタブ
 	}
 
 	public void draw(MyFrame frame) {
@@ -15,11 +12,6 @@ public class Train {
 		frame.fillOval(x + 15, y + 30, 10, 10);
 		frame.fillOval(x + 75, y + 30, 10, 10);
 		frame.fillOval(x + 85, y + 30, 10, 10);
-	}
-
-	public void move() {
-		x += vx;
-		y += vy;
 	}
 
 }
