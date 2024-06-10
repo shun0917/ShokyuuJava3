901cf7e3e76c542519ec232b2710f7735aa93c1e
髙木　瑛俊 <133090478+shun0917@users.noreply.github.com>
2024-06-10 12:06:20 +0900

    9.1元となる図形を定義する

---
+++ 初級Java2.5/src/PlayerAnimation.java
@@ -0,0 +1,16 @@
+
+public class PlayerAnimation extends MyFrame{
+	public void run() {
+		Car player=new Car(100,200,0,0);
+		while(true){
+			clear();
+			player.draw(this);
+			player.move();
+			sleep(0.1);
+			
+			
+			
+		}
+	}
+
+}
